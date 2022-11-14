e843c894218a5a1429c84a79aa8333cce70a8de9
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2022-11-14 16:37:51 +0100

    Implemented two buttons in the main activity. Also changed the color scheme. will probably change colors in the future.

--- app/src/main/java/com/example/higherorlower/MainActivity.kt
+++ app/src/main/java/com/example/higherorlower/MainActivity.kt
@@ -2,16 +2,26 @@ package com.example.higherorlower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    // Buttons on the main-screen
 
+    lateinit var playButton : Button
+    lateinit var exitButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        playButton = findViewById(R.id.playButton)
+        exitButton = findViewById(R.id.exitButton)
 
-        
+
+
+
+
+
     }
 }
